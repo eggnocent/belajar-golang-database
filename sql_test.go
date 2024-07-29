7d5ae07da05719d6d5e1af34704975c80b2e9615
eggnocent <egiwiratamaaa@gmail.com>
2024-07-29 15:55:57 +0700

    no query&query sql

--- sql_test.go
+++ sql_test.go
@@ -21,3 +21,27 @@ func TestExecSql(t *testing.T) {
 	}
 	fmt.Println("success import new customer")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id,name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id:", id)
+		fmt.Println("nama:", name)
+	}
+}
