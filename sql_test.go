400ddaa9838fc2ba833d109640b6383597f06c76
eggnocent <egiwiratamaaa@gmail.com>
2024-07-30 11:44:18 +0700

    sql injection safe

--- sql_test.go
+++ sql_test.go
@@ -118,3 +118,31 @@ func TestSqlInjection(t *testing.T) {
 		fmt.Println("Gagal login")
 	}
 }
+
+func TestSqlInjectionSave(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "EgiWira"
+	password := "aa"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("sukses login", username)
+	} else {
+		fmt.Println("Gagal login")
+	}
+}
